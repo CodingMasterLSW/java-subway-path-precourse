95f3606daacdb4c70bb0a071c5865731688758ca
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-12-11 20:02:18 +0900

    test: 노선 초기화 테스트코드 작성

--- src/test/java/subway/service/SubwayServiceTest.java
+++ src/test/java/subway/service/SubwayServiceTest.java
@@ -4,10 +4,11 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
@@ -24,15 +25,23 @@ public class SubwayServiceTest {
     @Test
     void subway_validation_test() {
         List<Station> stations = StationRepository.stations();
-
         List<String> compareStation = List.of("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
         List<String> validateStationNames = new ArrayList<>();
-
         for (Station station : stations) {
             validateStationNames.add(station.getName());
         }
-
         assertThat(compareStation.containsAll(validateStationNames));
     }
 
+    @DisplayName("노선 저장 검증 테스트")
+    @Test
+    void line_validation_test() {
+        List<Line> lines = LineRepository.lines();
+        List<String> compareLine = List.of("2호선", "3호선", "신분당");
+        List<String> validateLineNames = new ArrayList<>();
+        for (Line line : lines) {
+            validateLineNames.add(line.getName());
+        }
+        assertThat(compareLine.containsAll(validateLineNames));
+    }
 }
