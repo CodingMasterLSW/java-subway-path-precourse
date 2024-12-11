f22fbf70a511df1e32c55183b4674e7a028970fb
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-12-12 02:47:45 +0900

    test: 실행 최소시간 및 최소거리 테스트코드 작성

--- src/test/java/subway/service/SubwayServiceTest.java
+++ src/test/java/subway/service/SubwayServiceTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import subway.domain.CalculateResultDto;
 import subway.domain.Line;
 import subway.domain.LineRepository;
 import subway.domain.Station;
@@ -43,5 +44,34 @@ public class SubwayServiceTest {
             validateLineNames.add(line.getName());
         }
         assertThat(compareLine.containsAll(validateLineNames));
+    }
+
+    @DisplayName("교대역 - 양재역 최소시간 값은 9km, 7분 이어야 한다.")
+    @Test
+    void 교대_양재_실행_최소시간_테스트() {
+        CalculateResultDto calculateResultDto = subwayService.calculateDistance(true, "교대역", "양재역");
+        List<String> expectedResult = List.of("교대역", "남부터미널역", "양재역");
+        int distance = calculateResultDto.getDistance();
+        int time = calculateResultDto.getTime();
+        List<String> stations = calculateResultDto.getStations();
+        for (String station : stations) {
+            System.out.println(station);
+        }
+        assertThat(stations).containsExactlyInAnyOrderElementsOf(expectedResult);
+        assertThat(distance).isEqualTo(9);
+        assertThat(time).isEqualTo(7);
+    }
+
+    @DisplayName("교대역 - 양재역 최소거리 값은 4km, 11분 이어야 한다.")
+    @Test
+    void 교대_양재_실행_최소거리_테스트() {
+        CalculateResultDto calculateResultDto = subwayService.calculateDistance(false, "교대역", "양재역");
+        List<String> expectedResult = List.of("교대역", "강남역", "양재역");
+        List<String> stations = calculateResultDto.getStations();
+        int distance = calculateResultDto.getDistance();
+        int time = calculateResultDto.getTime();
+        assertThat(stations).containsExactlyInAnyOrderElementsOf(expectedResult);
+        assertThat(distance).isEqualTo(4);
+        assertThat(time).isEqualTo(11);
     }
 }
